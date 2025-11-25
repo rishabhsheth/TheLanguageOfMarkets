45836615348240b4d02619f46b3242dce1cecb36
Rishabh Sheth <shethrishabh1@gmail.com>
2025-11-25 16:53:44 -0500

    Create load_model.py to download the FinBERT model and run it locally

---
+++ FinBERT/load_model.py
@@ -0,0 +1,6 @@
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+
+model_name = "ProsusAI/finbert"
+
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForSequenceClassification.from_pretrained(model_name)
